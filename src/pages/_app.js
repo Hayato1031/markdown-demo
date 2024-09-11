b66a3992a8f6fd3c7e48eb2f513c78a244b476e6
Hayato1031 <nakamurahayato1031@keio.jp>
2024-09-11 15:09:53 +0900

    markdown

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,10 @@
 import "@/styles/globals.css";
+import { UIProvider } from "@yamada-ui/react"
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <UIProvider>
+      <Component {...pageProps} />
+    </UIProvider>
+  );
 }
